bb007ce0ab81e9ddf25d9f0fd67ea3266c6a76a1
Suraj Verma <5441349+SurajVerma@users.noreply.github.com>
2019-09-08 16:49:14 +0530

    Egghead
    Beginners Guide

--- src/index.js
+++ src/index.js
@@ -2,15 +2,56 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
+import { func } from "prop-types";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
+// const element = <div className="container">Hello World!</div>;
+// React.createElement(
+//   "div",
+//   { className: "container" },
+//   "hello world!!"
+// );
+// ReactDOM.render(element, root);
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+// (function tick() {
+//   const time = new Date().toLocaleTimeString();
+//   const element = <div>It is {time}</div>;
+//   ReactDOM.render(element, root);
+//   setInterval(tick, 1000);
+// })();
+
+// const state = { eventCount: 0, username: "" };
+
+// function App() {
+//   return (
+//     <div>
+//       <p>There have been {state.eventCount} events</p>
+//       <p>
+//         <button onClick={increment}>Click Here</button>
+//       </p>
+//       <p>You typed: {state.username}</p>
+//       <p>
+//         <input onChange={updateUsername}/>
+//       </p>
+//     </div>
+//   );
+// }
+
+// function increment() {
+//   setState({
+//     eventCount: state.eventCount + 1
+//   });
+// }
+// function updateUsername(event){
+//   setState({
+//     username : event.target.value
+//   })
+// }
+// function setState(newState) {
+//   Object.assign(state, newState);
+//   renderApp();
+// }
+
+// function renderApp() {
+//   ReactDOM.render(<App />, root);
+// }
+// renderApp();
