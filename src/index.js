e9786956dc898c9a0c378cbb3422e8f80b5203c4
Suraj Verma <5441349+SurajVerma@users.noreply.github.com>
2019-09-09 11:04:52 +0530

    removed unrelevant code
    Only the stopwatch left

--- src/index.js
+++ src/index.js
@@ -3,58 +3,6 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-// const element = <div className="container">Hello World!</div>;
-// React.createElement(
-//   "div",
-//   { className: "container" },
-//   "hello world!!"
-// );
-// ReactDOM.render(element, root);
-
-// (function tick() {
-//   const time = new Date().toLocaleTimeString();
-//   const element = <div>It is {time}</div>;
-//   ReactDOM.render(element, root);
-//   setInterval(tick, 1000);
-// })();
-
-// const state = { eventCount: 0, username: "" };
-
-// function App() {
-//   return (
-//     <div>
-//       <p>There have been {state.eventCount} events</p>
-//       <p>
-//         <button onClick={increment}>Click Here</button>
-//       </p>
-//       <p>You typed: {state.username}</p>
-//       <p>
-//         <input onChange={updateUsername}/>
-//       </p>
-//     </div>
-//   );
-// }
-
-// function increment() {
-//   setState({
-//     eventCount: state.eventCount + 1
-//   });
-// }
-// function updateUsername(event){
-//   setState({
-//     username : event.target.value
-//   })
-// }
-// function setState(newState) {
-//   Object.assign(state, newState);
-//   renderApp();
-// }
-
-// function renderApp() {
-//   ReactDOM.render(<App />, root);
-// }
-// renderApp();
-
 class StopWatch extends React.Component {
   state = { lapse: 0, running: false };
   handleRunClick = () => {
