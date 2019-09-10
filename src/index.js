1ce1ae842bfd03dd8e484ff43432f84c9c52776f
Suraj Verma <5441349+SurajVerma@users.noreply.github.com>
2019-09-10 15:46:52 +0530

    use class component with react
    use class component with react

--- src/index.js
+++ src/index.js
@@ -3,48 +3,67 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-class StopWatch extends React.Component {
-  state = { lapse: 0, running: false };
-  handleRunClick = () => {
-    this.setState(state => {
-      if (state.running) {
-        clearInterval(this.timer);
-      } else {
-        const startTime = Date.now() - this.state.lapse;
-        this.timer = setInterval(() => {
-          this.setState({ lapse: Date.now() - startTime });
-        });
-      }
-      return { running: !state.running };
-    });
-  };
-  handleClearClick = () => {
-    clearInterval(this.timer);
-    this.setState({ lapse: 0, running: false });
-  };
+// class StopWatch extends React.Component {
+//   state = { lapse: 0, running: false };
+//   handleRunClick = () => {
+//     this.setState(state => {
+//       if (state.running) {
+//         clearInterval(this.timer);
+//       } else {
+//         const startTime = Date.now() - this.state.lapse;
+//         this.timer = setInterval(() => {
+//           this.setState({ lapse: Date.now() - startTime });
+//         });
+//       }
+//       return { running: !state.running };
+//     });
+//   };
+//   handleClearClick = () => {
+//     clearInterval(this.timer);
+//     this.setState({ lapse: 0, running: false });
+//   };
+//   render() {
+//     const { lapse, running } = this.state;
+//     const buttonStyles = {
+//       border: "1px solid #ccc",
+//       background: "#fff",
+//       fontSize: "2em",
+//       padding: 15,
+//       margin: 5,
+//       width: 200
+//     };
+//     return (
+//       <div style={{ textAlign: "center" }}>
+//         <label style={{ fontSize: "5em", display: "block" }}> {lapse} ms</label>
+//         <button onClick={this.handleRunClick} style={buttonStyles}>
+//           {running ? "Stop" : "Start"}
+//         </button>
+//         <button onClick={this.handleClearClick} style={buttonStyles}>
+//           Clear
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+// const element = <StopWatch />;
+// ReactDOM.render(element, root);
+
+//Using class component with React
+class Counter extends React.Component {
+  constructor(...args) {
+    super(...args);
+    this.state = { count: 0 };
+  }
+  handleClick() {
+    this.setState(({ count }) => ({
+      count: count + 1
+    }));
+  }
   render() {
-    const { lapse, running } = this.state;
-    const buttonStyles = {
-      border: "1px solid #ccc",
-      background: "#fff",
-      fontSize: "2em",
-      padding: 15,
-      margin: 5,
-      width: 200
-    };
     return (
-      <div style={{ textAlign: "center" }}>
-        <label style={{ fontSize: "5em", display: "block" }}> {lapse} ms</label>
-        <button onClick={this.handleRunClick} style={buttonStyles}>
-          {running ? "Stop" : "Start"}
-        </button>
-        <button onClick={this.handleClearClick} style={buttonStyles}>
-          Clear
-        </button>
-      </div>
+      <button onClick={this.handleClick.bind(this)}>{this.state.count}</button>
     );
   }
 }
-
-const element = <StopWatch />;
-ReactDOM.render(element, root);
+ReactDOM.render(<Counter />, document.getElementById("root"));
